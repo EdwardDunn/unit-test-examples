703386b1a9ceb15203426dde9ec8a4ba5c295c37
EdwardDunn <edwarddunn12@gmail.com>
2018-11-19 16:17:35 +0000

    added Calculator class and initial test

---
+++ src/main/java/edward_dunn/unit_test_examples/Calculator.java
@@ -0,0 +1,6 @@
+package edward_dunn.unit_test_examples;
+
+public class Calculator {
+
+	
+}
---
+++ src/test/java/edward_dunn/unit_test_examples/CalculatorTests.java
@@ -0,0 +1,33 @@
+package edward_dunn.unit_test_examples;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class CalculatorTests extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	@Test
+	public void test_add_GivenPositiveNum_ReturnCorrectResult() {
+		//arrange
+		int num1 = 5;
+		int num2 = 6;
+		int expectedResult = 11;
+		int actualResult = 0;
+		
+		//act
+		Calculator calculator = new Calculator();
+		actualResult = calculator.add(num1, num2);
+		
+		//assert
+		assertEquals(expectedResult, actualResult);
+	}
+
+}
