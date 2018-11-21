4ef4bd4fb13c9d510a01face0966d08d9ec39375
EdwardDunn <edwarddunn12@gmail.com>
2018-11-21 15:09:46 +0000

    added Mockito maven dependency

---
+++ src/test/java/edward_dunn/unit_test_examples/CalculatorMockTests.java
@@ -0,0 +1,24 @@
+package edward_dunn.unit_test_examples;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorMockTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
