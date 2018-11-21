d2df1375f6a6124dda73a18c347f21bed0bc4111
EdwardDunn <edwarddunn12@gmail.com>
2018-11-21 14:57:51 +0000

    added multiply and divide functions to calculator class

--- src/main/java/edward_dunn/unit_test_examples/Calculator.java
+++ src/main/java/edward_dunn/unit_test_examples/Calculator.java
@@ -15,4 +15,12 @@ public class Calculator {
 	public int subtract(int value1, int value2) {
 		return value1 - value2;
 	}
+	
+	public int multiply(int value1, int value2) {
+		return value1 * value2;
+	}
+	
+	public int divide(int value1, int value2) {
+		return value1 / value2;
+	}
 }
