5a9c91e4f120e337d973d41238a502b4a855f53d
EdwardDunn <edwarddunn12@gmail.com>
2018-11-21 15:22:27 +0000

    implemented UnitsConverter class and ConverterService

--- src/main/java/edward_dunn/unit_test_examples/UnitsConverter.java
+++ src/main/java/edward_dunn/unit_test_examples/UnitsConverter.java
@@ -1,5 +1,20 @@
 package edward_dunn.unit_test_examples;
 
-public class UnitsConverter {
+public class UnitsConverter implements ConverterService {
+
+	public double convertGramsToKG(int grams) {
+		// TODO Auto-generated method stub
+		return grams / 1000;
+	}
+
+	public double convertPoundsToStone(int pounds) {
+		// TODO Auto-generated method stub
+		return pounds / 14;
+	}
+
+	public double convertFeetToYards(int feet) {
+		// TODO Auto-generated method stub
+		return feet / 3;
+	}
 
 }
