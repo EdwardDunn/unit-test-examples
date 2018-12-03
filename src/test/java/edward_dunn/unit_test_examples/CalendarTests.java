b84de4e686a450624b21522b45d7e552fb80b585
EdwardDunn <edwarddunn12@gmail.com>
2018-12-03 15:20:51 +0000

    added CalendaTests class with initial testing

---
+++ src/test/java/edward_dunn/unit_test_examples/CalendarTests.java
@@ -0,0 +1,53 @@
+package edward_dunn.unit_test_examples;
+
+import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalendarTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test_getCurrentDate_GivenNothing_ReturnCurrentDate() {
+		// arrange
+		String expectedResult = LocalDate.now().toString();
+		String actualResult = "";
+		
+		// act
+		Calendar cal = new Calendar();
+		actualResult = cal.getCurrentDate();
+		
+		// assert 
+		assertEquals(expectedResult, actualResult);		
+	}
+	
+	@Test
+	public void test_getCurrentDayOfWeek_GivenNothing_ReturnCurrentDayOfWeek() {
+		// arrange
+        Date now = new Date();
+        SimpleDateFormat simpleDateformat = new SimpleDateFormat("EEEE");		
+		String expectedResult = simpleDateformat.format(now);
+		String actualResult = "";
+		
+		// act
+		Calendar cal = new Calendar();
+		actualResult = cal.getCurrentDay();
+		
+		// assert 
+		assertEquals(expectedResult, actualResult);		
+	}
+
+}
