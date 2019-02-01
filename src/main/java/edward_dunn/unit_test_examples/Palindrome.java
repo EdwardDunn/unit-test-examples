318432cceb219d592e5014a19aa8e6106340769f
EdwardDunn <1505629@uad.ac.uk>
2019-02-01 16:00:23 +0000

    added additional palindrome tests

--- src/main/java/edward_dunn/unit_test_examples/Palindrome.java
+++ src/main/java/edward_dunn/unit_test_examples/Palindrome.java
@@ -3,6 +3,12 @@ package edward_dunn.unit_test_examples;
 public class Palindrome {
 
 	public boolean isPalindrome(String word){
+		if(word.equals("")) {
+			return false;
+		}
+		
+		word.toLowerCase();
+		
     	StringBuilder sb = new StringBuilder(word);
     	String reversed = sb.reverse().toString();
     	
--- src/test/java/edward_dunn/unit_test_examples/tests/PalindromeTests.java
+++ src/test/java/edward_dunn/unit_test_examples/tests/PalindromeTests.java
@@ -17,10 +17,46 @@ public class PalindromeTests extends TestCase {
 	}
 	
 	@Test
-	public void test_isPalindrome_GivenPalindrom_ReturnTrue(){
+	public void test_isPalindrome_GivenPalindrome_ReturnTrue(){
 		Palindrome pc = new Palindrome();
 		String word = "madam";
 		boolean expected = true;
+		boolean actual = pc.isPalindrome(word);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void test_isPalindrome_GivenUpperCasePalindrome_ReturnTrue(){
+		Palindrome pc = new Palindrome();
+		String word = "NOON";
+		boolean expected = true;
+		boolean actual = pc.isPalindrome(word);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void test_isPalindrome_GivenNonPalindrome_ReturnTrue(){
+		Palindrome pc = new Palindrome();
+		String word = "test";
+		boolean expected = false;
+		boolean actual = pc.isPalindrome(word);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void test_isPalindrome_GivenEmptyString_ReturnTrue(){
+		Palindrome pc = new Palindrome();
+		String word = "";
+		boolean expected = false;
+		boolean actual = pc.isPalindrome(word);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void test_isPalindrome_GivenSentence_ReturnTrue(){
+		Palindrome pc = new Palindrome();
+		String word = "The purpose of this test is to ensure sentences are handled correctly";
+		boolean expected = false;
 		boolean actual = pc.isPalindrome(word);
 		assertEquals(expected, actual);
 	}
